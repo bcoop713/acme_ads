3148199f7d75d07f153dba991d7c17cf2d214f5e
Bobby <bcoop713@gmail.com>
2014-10-15 14:10:28 -0400

    added foundation css framework

--- acme_ads/functional_tests/tests.py
+++ acme_ads/functional_tests/tests.py
@@ -1,8 +1,8 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 
 
-class AdManagementTest(LiveServerTestCase):
+class AdManagementTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
